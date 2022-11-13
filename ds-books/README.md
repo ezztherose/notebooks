eeb1d1e51d6ea3f77dc58badc1f51fa050faf3dc
Roseri <erikrosvall96@gmail.com>
2022-11-13 16:38:01 +0100

    still lxml problem

--- ds-books/README.md
+++ ds-books/README.md
@@ -2,4 +2,3 @@
 
 This is a data set from kaggle, with approximatly 1k different books regarding the topic "Data Science". The data does contain several attributes that can be uses to do many differet kinds of fun stuff with.
 
-The class in ```utility``` do not work for some reason. The program gives ``` Couldn't find a tree builder with the features you requested: lxml. Do you need to install a parser librar``` error. 
--- utility/amazon_scraper.py
+++ utility/amazon_scraper.py
@@ -3,11 +3,12 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from datetime import datetime
 import logging
+import lxml
 
 class Amazon_Scraper:
-    def __init__(self, URLS, df_reviews):
+    def __init__(self, URLS, titles):
         self.URLS = URLS
-        self.df_reviews = df_reviews
+        self.titles = titles
     
     def _get_headers(self):
         headers = {
@@ -47,8 +48,8 @@ class Amazon_Scraper:
 
     def order_data_gather(self, single_review: BeautifulSoup) -> dict:
         return {
-            'review_text': self.get_review_text(),
-            'review_stars': self.get_num_stars()
+            'review_text': self.get_review_text(single_review),
+            'review_stars': self.get_num_stars(single_review)
             }
 
     def data_to_csv(self):
@@ -59,9 +60,9 @@ class Amazon_Scraper:
             logging.info(self.URLS[i])
             html = self.get_html_page(self.URLS[i])
             reviews = self.get_html_reviews(html)
-            for r in reviews:
-                data = self.order_data_gather(r)
-                data.update({'title':self.df_reviews['title'][i]})
+            for rev in reviews:
+                data = self.order_data_gather(rev)
+                data.update({'title': self.titles['title'][i]})
                 results.append(data)
         
         from datetime import datetime
@@ -71,4 +72,5 @@ class Amazon_Scraper:
         logging.info(f'saving to {save_name}')
         out.to_csv(save_name, index=False)
         logging.info('Complete...')
+        return out
     
